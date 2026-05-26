338fbfb4fc59daf799e3b495f9a5c0516587006c
Konrad Sałkowski <konrad.salkowski@gmail.com>
2026-05-26 23:51:27 +0200

    Initial project setup

---
+++ src/Shy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while(true)
+        {
+            // READ
+
+            // EVAL
+
+            // PRINT
+
+            // LOOP
+        }
+    }
+}
