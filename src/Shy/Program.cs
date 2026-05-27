9b10c36cac54362cbc59b247493ac8f807f9c565
Konrad Sałkowski <konrad.salkowski@gmail.com>
2026-05-27 11:54:51 +0200

    Add basic prompt, exit and echo commands

--- src/Shy/Program.cs
+++ src/Shy/Program.cs
@@ -8,9 +8,30 @@ public class Program
     {
         while(true)
         {
+            // PROMPT
+            Console.Write("shy> ");
+
             // READ
+            var command = Console.ReadLine();
+            if(string.IsNullOrEmpty(command))
+            {
+                continue;
+            }
 
             // EVAL
+            if (command == "exit")
+            {
+                break;
+            }
+
+            if (command.StartsWith("echo "))
+            {
+                var echoMessage = command.Substring(5);
+                Console.WriteLine(echoMessage);
+                continue;
+            }
+
+            Console.WriteLine($"{command}: command not found");
 
             // PRINT
 
